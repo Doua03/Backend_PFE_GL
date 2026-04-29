0cee77fcce8ca7c7822358dfc61a2c6046add297
Doua03 <147230923+Doua03@users.noreply.github.com>
2026-04-29 16:07:17 +0100

    feat: Add OCL constraints for Parking model

--- model/parking.model.js
+++ model/parking.model.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const db = require('../config/db');
 const Schema = mongoose.Schema;
-const { applyParkingConstraints } = require('../constraints/ocl.constraints');
+const { applyParkingConstraints } = require('../constraints/parking.constraints');
 
 const parkingPlaceSchema = new Schema({
   floorIndex: String,
@@ -65,41 +65,9 @@ const parkingSchema = new Schema({
   reservedPlacesCount: Number
 });
 
-// Apply OCL Constraints to the parking schema
+// Apply Parking Constraints to the parking schema
 applyParkingConstraints(parkingSchema);
 
 const Parking = db.model('Parking', parkingSchema);
-
-// Mettre à jour les compteurs après chaque modification de place de parking
-parkingPlaceSchema.post('save', async function(doc) {
-  try {
-    const parkingId = this.parent().parent().parent()._id;
-    
-    const parking = await Parking.findById(parkingId);
-    let occupiedCount = 0;
-    let reservedCount = 0;
-    
-    for (const floor of parking.floors) {
-      for (const place of floor.places) {
-        if (place.status === false) {
-          occupiedCount++;
-        } else {
-          reservedCount++;
-        }
-      }
-    }
-
-    parking.occupiedPlacesCount = occupiedCount;
-    parking.reservedPlacesCount = reservedCount;
-
-    await parking.save();
-  } catch (error) {
-    console.error('Error updating parking counters:', error);
-  }
-});
-
-
-
-
 
 module.exports = Parking;
